d47ab07df0061dc4c79967be571b33c1728347a0
ewanwong <903327884@qq.com>
2023-07-24 21:42:59 +0200

    commit

--- try.py
+++ try.py
@@ -5,24 +5,30 @@ import torch.nn as nn
 from models.PrefixGPT2 import PrefixGPT2
 from models.training_utils import get_conditional_prob_dist
 from models.training_utils import get_sent_prob_diff_loss, KLD
-config = AutoConfig.from_pretrained('gpt2')
-config.pre_seq_len = 10
-config.prefix_hidden_size = 800
-config.prefix_projection = True
-config.hidden_dropout_prob = 0.1
-model = PrefixGPT2(config).cuda()
-tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-tokenizer.pad_token = tokenizer.eos_token
-kld_model = KLD()
 
-prompts = ['The doctor performing surgery is a young','The doctor performing surgery is a young', 'The criminal is a black', 'i hate dog']
-# print(get_sent_prob_diff_loss(model, tokenizer, prompts, kld_model))
-word_list = ['man', 'woman']
-prob_dist = get_conditional_prob_dist(model, tokenizer, prompts, word_list, 4)
-print(prob_dist / torch.sum(prob_dist,dim=1).unsqueeze(1).expand(-1, prob_dist.shape[1]))
-prob_dist = get_conditional_prob_dist(model, tokenizer, prompts, word_list, 3)
-print(prob_dist / torch.sum(prob_dist,dim=1).unsqueeze(1).expand(-1, prob_dist.shape[1]))
-prob_dist = get_conditional_prob_dist(model, tokenizer, prompts, word_list, 2)
-print(prob_dist / torch.sum(prob_dist,dim=1).unsqueeze(1).expand(-1, prob_dist.shape[1]))
-prob_dist = get_conditional_prob_dist(model, tokenizer, prompts, word_list, 1)
-print(prob_dist / torch.sum(prob_dist,dim=1).unsqueeze(1).expand(-1, prob_dist.shape[1]))
+
+model = PrefixGPT2.from_pretrained("/home/CE/yifwang/master_thesis/debias_NLG/fast_model_use_full_words_True_alpha1_25_alpha2_50_alpha3_25_alpha4_100_beta_10_pre_seq_len_10_hidden_size_800_batch_size_16_lm_batch_size_128_lr_5e-05_lm_lr_5e-05_epochs_2_lm_epochs_0_warmup_500_lm_warmup_100_random_seed_42/epoch_2")
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+
+input_text = "The doctor is a young"
+input = tokenizer(input_text, return_tensors='pt')
+past_key_values =  model(**input, use_cache=True).past_key_values
+
+# w/o past_k_v
+input_text = "The doctor is a young black"
+input = tokenizer(input_text, return_tensors='pt')
+output = model(**input)
+print(output.logits[-1])
+# w past_k_v
+input_text = "black"
+input = tokenizer(input_text, return_tensors='pt')
+output =  model(**input, past_key_values=past_key_values)
+print(output.logits[-1])
+# w/o past_k_v
+input = tokenizer(input_text, return_tensors='pt')
+output =  model(**input)
+print(output.logits[-1])
+# w past_k_v and mask=0
+mask_length = past_key_values[0][0].shape[-2]
+output =  model(**input)
+print(output.logits[-1])
